5d11000d170a307ed24069f25630042c57c97b16
Andrew Furrer <afurrer143@gmail.com>
2023-01-04 16:01:22 -0600

    methodNotAllowed Error Handling

--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -1,10 +1,12 @@
 const router = require("express").Router()
 const controller = require("./reviews.controller")
+const methodNotAllowed = require("../utils/methodNotAllowed")
 
-router.route("/").get(controller.list)
+router.route("/").get(controller.list).all(methodNotAllowed)
 router.route("/:reviewId")
     .get(controller.read)
     .put(controller.update)
     .delete(controller.delete)
+    .all(methodNotAllowed)
 
 module.exports = router
--- src/theaters/theater.router.js
+++ src/theaters/theater.router.js
@@ -1,6 +1,7 @@
 const router = require("express").Router()
 const controller = require("./theaters.controller")
+const methodNotAllowed = require("../utils/methodNotAllowed")
 
-router.route("/").get(controller.list)
+router.route("/").get(controller.list).all(methodNotAllowed)
 
 module.exports = router
