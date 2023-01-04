fb4dd66c3d13d8026eb3d37e122f9fe11548acea
Andrew Furrer <afurrer143@gmail.com>
2023-01-04 15:45:24 -0600

    cors updated

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const moviesRouter = require("./movies/movies.router")
 const reviewsRouter = require("./reviews/reviews.router")
 const theatersRouter = require("./theaters/theater.router")
 
 app.use(express.json())
+
+app.use(cors());
 
 app.use("/movies", moviesRouter)
 
